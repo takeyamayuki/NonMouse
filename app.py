e6fb77388f8bb478725ea0ed8b2517fca622b55e
takeyamayuki <namiki.takeyama@gmail.com>
2021-08-12 02:00:48 +0900

    update app.py

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ mouse = Controller()
 mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
 
-def main(): 
+
+def main():
     # マウス感度（大きくすると、小刻みに動きやすくなるので、同時にranも大きくする）
     kando = 1.5
     # スムージング量（小さいとカーソルが小刻みに動きやすくなるが、大きいと遅延が大きくなる）
@@ -35,18 +36,18 @@ def main():
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 
     with mp_hands.Hands(
-            min_detection_confidence=0.7,
-            min_tracking_confidence=0.7,
-            max_num_hands=1) as hands:
+        min_detection_confidence=0.7,
+        min_tracking_confidence=0.7,
+        max_num_hands=1) as hands:      # handsオブジェクトとしてmp_hands.Handsを読み込む
+
         while cap.isOpened():
             success, image = cap.read()
             if not success:
                 print("Ignoring empty camera frame.")
-                # ビデオをロードする場合は、「continue」ではなく「break」を使用
                 break
-            # 後で自分撮りビューを表示するために画像を水平方向に反転し、BGR画像をRGBに変換
+            # 画像を水平方向に反転し、BGR画像をRGBに変換
             image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
-            # パフォーマンスを向上させるために、オプションで、参照渡しのためにイメージを書き込み不可としてマーク
+            # 参照渡しのためにイメージを書き込み不可としてマーク
             image.flags.writeable = False
             results = hands.process(image)
             # 画像に手のアノテーションを描画
@@ -59,18 +60,17 @@ def main():
                     mp_drawing.draw_landmarks(
                         image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
                     #print('hand_landmarks:', hand_landmarks)
-
                     # 人差し指の先端と中指の先端間のユークリッド距離
                     Ugo = (hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x * image_width -
-                        hand_landmarks.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_TIP].x * image_width,
-                        hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y * image_height -
-                        hand_landmarks.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_TIP].y * image_height)
+                           hand_landmarks.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_TIP].x * image_width,
+                           hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y * image_height -
+                           hand_landmarks.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_TIP].y * image_height)
                     absUgo = np.linalg.norm(Ugo)
                     # 人差し指の第２関節と親指の先端間のユークリッド距離
                     Cli = (hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_PIP].x * image_width -
-                        hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_TIP].x * image_width,
-                        hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_PIP].y * image_height -
-                        hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_TIP].y * image_height)
+                           hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_TIP].x * image_width,
+                           hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_PIP].y * image_height -
+                           hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_TIP].y * image_height)
                     absCli = np.linalg.norm(Cli)
                     # 移動量平均によるスムージング
                     # 末尾に追加
@@ -117,6 +117,7 @@ def main():
             if cv2.waitKey(5) & 0xFF == 27:
                 break
     cap.release()
+
 
 if __name__ == "__main__":
     main()

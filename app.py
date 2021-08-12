37ee601f19145ffe1c0b595e58b6512418b45cc2
takeyamayuki <namiki.takeyama@gmail.com>
2021-08-12 17:32:33 +0900

    Fixed a bug in the initial mouse cursor value

--- app.py
+++ app.py
@@ -2,24 +2,25 @@ import argparse
 import cv2
 import mediapipe as mp
 import numpy as np
+from numpy.testing._private.utils import jiffies
 from pynput.mouse import Button, Controller
 mouse = Controller()
 mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
 
-
 def main():
     # マウス感度（大きくすると、小刻みに動きやすくなるので、同時にranも大きくする）
     kando = 1.5
     # スムージング量（小さいとカーソルが小刻みに動きやすくなるが、大きいと遅延が大きくなる）
-    ran = 5
+    ran = 3
     # タッチ距離（遠いほど小さく、近いほど大きい値にする）
     dis = 65
     preX, preY = 0, 0
     preCli = 0
     douCli = 0
-    LiTx = [0, 0, 0, 0, 0]
-    LiTy = [0, 0, 0, 0, 0]
+    LiTx = []
+    LiTy = []
+    i=0
     # 引数
     parser = argparse.ArgumentParser()
     parser.add_argument("--device", type=int, default=0)
@@ -34,16 +35,15 @@ def main():
     cap = cv2.VideoCapture(cap_device)
 
     hands = mp_hands.Hands(
-            min_detection_confidence=0.7,   # 検出信頼度
-            min_tracking_confidence=0.7,    # 追跡信頼度
-            max_num_hands=1                 # 最大検出数
+        min_detection_confidence=0.7,   # 検出信頼度
+        min_tracking_confidence=0.7,    # 追跡信頼度
+        max_num_hands=1                 # 最大検出数
     )
 
     while cap.isOpened():
         success, image = cap.read()
         if not success:
-            print("Ignoring empty camera frame.")
-            break
+            continue
         # 画像を水平方向に反転し、BGR画像をRGBに変換
         image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
         # 参照渡しのためにイメージを書き込み不可としてマーク
@@ -58,32 +58,41 @@ def main():
             for hand_landmarks in results.multi_hand_landmarks:
                 mp_drawing.draw_landmarks(
                     image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
-                #print('hand_landmarks:', hand_landmarks)
+                
+                # preX, preY, LiTx, LiTyの初期値に現在のマウス位置を代入 1回だけ実行
+                if i==0:
+                    preX=hand_landmarks.landmark[8].x * image_width
+                    preY=hand_landmarks.landmark[8].y * image_height
+                    for j in range(ran):
+                        LiTx.append(hand_landmarks.landmark[8].x * image_width)
+                        LiTy.append(hand_landmarks.landmark[8].y * image_height)
+                    i=+1
+
+                # print('hand_landmarks:', hand_landmarks.landmark[8].x)
                 # 人差し指の先端と中指の先端間のユークリッド距離
-                Ugo = (hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x * image_width -
-                        hand_landmarks.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_TIP].x * image_width,
-                        hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y * image_height -
-                        hand_landmarks.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_TIP].y * image_height)
+                Ugo = (hand_landmarks.landmark[8].x * image_width - hand_landmarks.landmark[12].x * image_width,
+                       hand_landmarks.landmark[8].y * image_height - hand_landmarks.landmark[12].y * image_height)
                 absUgo = np.linalg.norm(Ugo)
                 # 人差し指の第２関節と親指の先端間のユークリッド距離
-                Cli = (hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_PIP].x * image_width -
-                        hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_TIP].x * image_width,
-                        hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_PIP].y * image_height -
-                        hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_TIP].y * image_height)
+                Cli = (hand_landmarks.landmark[6].x * image_width - hand_landmarks.landmark[4].x * image_width,
+                       hand_landmarks.landmark[6].y * image_height - hand_landmarks.landmark[4].y * image_height)
                 absCli = np.linalg.norm(Cli)
+                # 中指の先端と薬指の先端間のユークリッド距離
+                Scr = (hand_landmarks.landmark[12].x * image_width - hand_landmarks.landmark[16].x * image_width,
+                       hand_landmarks.landmark[12].y * image_height - hand_landmarks.landmark[16].y * image_height)
+                absScr = np.linalg.norm(Scr)
+
                 # 移動量平均によるスムージング
                 # 末尾に追加
-                LiTx.append(
-                    hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x * image_width)
-                LiTy.append(
-                    hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y * image_height)
+                LiTx.append(hand_landmarks.landmark[8].x * image_width)
+                LiTy.append(hand_landmarks.landmark[8].y * image_height)
                 if len(LiTx) > ran:
-                    # 先頭を削除
-                    LiTx.pop(0)
+                    LiTx.pop(0)     # 先頭を削除
                     LiTy.pop(0)
                 # カメラ座標をマウス移動量に変換
-                dx = kando * (sum(LiTx)/len(LiTx) - preX)
-                dy = kando * (sum(LiTy)/len(LiTy) - preY)
+                dx = kando * (sum(LiTx)/ran - preX)
+                dy = kando * (sum(LiTy)/ran - preY)
+
                 # click状態
                 if absCli < dis:
                     nowCli = 1
@@ -96,7 +105,6 @@ def main():
                         mouse.move(dx, -dy)
                     if args.direction == 1:
                         mouse.move(dx, dy)
-                    # print('Move')
                 # click
                 if nowCli == 1 and nowCli != preCli:
                     mouse.press(Button.left)
@@ -108,8 +116,8 @@ def main():
                     douCli += 1
                     print('Release')
 
-                preX = sum(LiTx)/len(LiTx)
-                preY = sum(LiTy)/len(LiTy)
+                preX = sum(LiTx)/ran
+                preY = sum(LiTy)/ran
                 preCli = nowCli
 
         cv2.imshow('NonMouse', image)

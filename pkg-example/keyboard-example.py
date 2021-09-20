ece62db5f65046f882077757aa6cb5e4373cd4fb
Yuki Takeyama <namiki.takeyama@gmail.com>
2021-09-20 10:42:41 +0900

    add samples

--- pkg-example/keyboard-example.py
+++ pkg-example/keyboard-example.py
@@ -1,6 +1,11 @@
 import keyboard
-hotkey='shift+space'
+hotkey='option'
 
 while True:
     keyboard.wait(hotkey)
     print('hotkey was pressed! Waiting on it again...')
+
+
+# while True:
+#     if keyboard.is_pressed(hotkey):
+#         print('hotkey was pressed! Waiting on it again...')

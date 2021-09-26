543473ae9b731d189a178fcdedc7c16d255eee6d
Yuki Takeyama <namiki.takeyama@gmail.com>
2021-09-26 16:09:08 +0900

    add samples

---
+++ pkg-example/opencv-watikey-research/test.py
@@ -0,0 +1,11 @@
+import cv2
+img = cv2.imread('m.png') # load a dummy image
+while(1):
+    cv2.imshow('img',img)
+    k = cv2.waitKey(33)
+    if k==27:    # Esc key to stop
+        break
+    elif k==-1:  # normally -1 returned,so don't print it
+        continue
+    else:
+        print(k) # else print its value
